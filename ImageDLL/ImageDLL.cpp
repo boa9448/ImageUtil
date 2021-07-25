4b484c4650004728076cd25c9a1af85f6cfad3b0
WDW <boa3465@gmail.com>
2021-07-26 02:11:47 +0900

    프로젝트 생성

---
+++ ImageDLL/ImageDLL.cpp
@@ -0,0 +1,22 @@
+﻿// ImageDLL.cpp : DLL을 위해 내보낸 함수를 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ImageDLL.h"
+
+
+// 내보낸 변수의 예제입니다.
+IMAGEDLL_API int nImageDLL=0;
+
+// 내보낸 함수의 예제입니다.
+IMAGEDLL_API int fnImageDLL(void)
+{
+    return 0;
+}
+
+// 내보낸 클래스의 생성자입니다.
+CImageDLL::CImageDLL()
+{
+    return;
+}
