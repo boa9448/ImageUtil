fd630a87e27a39cdf1a32e14f1a5d30edf493343
boa9448 <boa3465@gmail.com>
2021-08-02 13:01:14 +0900

    c# 테스트에 시간 체크 추가

--- ImageDLLTest_CSharp/Program.cs
+++ ImageDLLTest_CSharp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using Image;
 
 namespace ImageDLLTest_CSharp
@@ -13,8 +14,12 @@ namespace ImageDLLTest_CSharp
         static void ImageSearchExTest()
         {
             RECT find = new RECT();
+            var watch = new Stopwatch();
+            watch.Start();
             int nRet = Search.ImageSearchEx("..\\..\\..\\imgs\\src.png", "..\\..\\..\\imgs\\temp.png", ref find, 255);
+            watch.Stop();
 
+            Console.WriteLine("search time : {0} ms", watch.ElapsedMilliseconds);
             Console.WriteLine("find count : {0}", nRet);
             Console.WriteLine("x : {0}, y : {1}, w : {2}, h : {3}"
                 , find.left, find.top, find.right - find.left, find.bottom - find.top);
@@ -23,10 +28,14 @@ namespace ImageDLLTest_CSharp
         static void ImageSearchEx_AllTest()
         {
             RECT[] find_list = new RECT[50];
+            var watch = new Stopwatch();
+            watch.Start();
             int nRet = Search.ImageSearchEx_All("..\\..\\..\\imgs\\src2.png", "..\\..\\..\\imgs\\temp2.png"
                 , find_list, (uint)find_list.Length, 255);
-            Console.WriteLine("find count : {0}", nRet);
+            watch.Stop();
 
+            Console.WriteLine("search time : {0} ms", watch.ElapsedMilliseconds);
+            Console.WriteLine("find count : {0}", nRet);
             for(int idx = 0; idx < nRet; idx++)
             {
                 Console.WriteLine("x : {0}, y : {1}, w : {2}, h : {3}"
@@ -37,13 +46,18 @@ namespace ImageDLLTest_CSharp
 
         static void Main(string[] args)
         {
-            var value = System.Environment.GetEnvironmentVariable("PATH");
-            value += ";E:\\source\\repos\\ImageUtil\\x64\\Debug";
-            value += ";E:\\Library\\opencv\\install\\x64\\vc16\\bin";
+            var value = System.Environment.GetEnvironmentVariable("PATH") + ";";
+            value += System.IO.Path.GetFullPath("..\\..\\..\\x64\\Debug") + ";";
+            value += System.IO.Path.GetFullPath("..\\..\\..\\x64\\vc16\\bin") + ";";
+            value += "F:\\WDW\\Library\\opencv\\build\\x64\\vc15\\bin;";
             System.Environment.SetEnvironmentVariable("PATH", value);
 
-            ImageSearchExTest();
-            ImageSearchEx_AllTest();
+            for (int idx = 0; idx < 10; idx++)
+            {
+                Console.WriteLine("==========================================================");
+                ImageSearchExTest();
+                ImageSearchEx_AllTest();
+            }
         }
     }
 }
